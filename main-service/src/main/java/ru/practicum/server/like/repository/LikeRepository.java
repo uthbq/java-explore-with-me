7a8e6b51eab4f2ae796fd33d6183658e2654e1ed
Georgii <moroz.georgijs@yandex.ru>
2024-09-19 11:01:51 +0300

    2

--- main-service/src/main/java/ru/practicum/server/like/repository/LikeRepository.java
+++ main-service/src/main/java/ru/practicum/server/like/repository/LikeRepository.java
@@ -2,6 +2,9 @@ package ru.practicum.server.like.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Map;
 import ru.practicum.server.like.model.LikeInfo;
 import ru.practicum.server.like.model.LikeInfoId;
 
